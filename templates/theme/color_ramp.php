73d34aa78118ec8fc9fd2deb794c49daa3f9ce79
Cuesta <itanki3085@gmail.com>
2023-05-06 23:08:41 +0800

    找個模板、挑出會用到的網頁、套色票、改字樣。

---
+++ templates/theme/color_ramp.php
@@ -0,0 +1,20 @@
+<?php
+echo"
+<style>
+/* 溫暖色票 */
+:root {
+  ---lightyellow: #F6D9B1;
+  ---yellow: #F6CC8C;
+  ---brown: #D4A379;
+  ---skin: #EBAD91;
+  ---rose: #D48879;
+  ---red: #F68F8C;
+
+  ---blue: #79B4F2;
+  ---lightblue: #7EDDFC;
+  ---cyan: #7FE5E4;
+  ---lightcyan: #7EFCD7;
+  ---green: #79F2AC;
+}
+</style>";
+?>
